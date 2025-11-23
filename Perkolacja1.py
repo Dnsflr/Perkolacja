de175bef24c3cb8a4122677e21643298b2dfb08c
Gabriel Tyszka <Gabriel.Tyszka09@gmail.com>
2025-11-23 12:24:10 +0100

    Update Perkolacja1.py

--- Perkolacja1.py
+++ Perkolacja1.py
@@ -2,10 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 
+L = 20 
+p = 0.59
+
+
 def pbc(j, L):
     return j % L
 
 def initialize_grid(L):
     lattice = np.ones((L, L)) * (-1)
     
-            
+    #brzegi
+    lattice[0,:] = -2
+    lattice[-1,:] = -2
+    
+    
+    return lattice
+    
+
+
+lattice = initialize_grid(L)
+
+for i in range(L):
+    for j in range(L):
+        pass
+
+
+plt.imshow(lattice, interpolation= 'nearest', cmap= 'magma')
+
+plt.show()
