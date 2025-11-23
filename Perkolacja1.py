f6a780458fa9e7e205c4a100a577798a254b095b
Gabriel Tyszka <Gabriel.Tyszka09@gmail.com>
2025-11-23 13:21:55 +0100

    chyba kumam

--- Perkolacja1.py
+++ Perkolacja1.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
+import random
 
 L = 20 
 p = 0.59
+q = deque()
 
 
 def pbc(j, L):
@@ -15,18 +17,29 @@ def initialize_grid(L):
     #brzegi
     lattice[0,:] = -2
     lattice[-1,:] = -2
+    lattice[1,:] = 1
     
+    for i in range(L):
+        for j in range(L):
+            if i == 1:
+                q.append([i,j])
+            else:
+                pass
     
     return lattice
     
-
-
 lattice = initialize_grid(L)
 
-for i in range(L):
-    for j in range(L):
-        pass
-
+licznik = 0 
+while q:
+    x_index, y_index = q[0]
+    
+    for i in range(4):
+        if (lattice[x_index, pbc(y_index + 1)] != -1):
+            pass
+    
+    q.popleft()
+    
 
 plt.imshow(lattice, interpolation= 'nearest', cmap= 'magma')
 
