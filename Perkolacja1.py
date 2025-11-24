765a9bf6f20b8d37a1101c8991e1aa3f4aa0c11f
Gabriel Tyszka <Gabriel.Tyszka09@gmail.com>
2025-11-24 09:54:10 +0100

    Revert "Update Perkolacja1.py"
    This reverts commit 5be683f823f0d565cc93bef61e3ce2ff93e28073.

--- Perkolacja1.py
+++ Perkolacja1.py
@@ -7,34 +7,31 @@ L = 100
 p = 0.59
 q = deque()
 
-# pomocnicza globalna zmienna na losowanie
-random_num = 0.0
-
 
 def pbc(j):
     return j % L
 
-
-def initialize_grid(L_local):
-    global q
-    lattice_local = np.ones((L_local, L_local)) * (-1)
+def initialize_grid(L):
+    lattice = np.ones((L, L)) * (-1)
     
-    lattice_local[0, :] = -2
-    lattice_local[-1, :] = -2
-    lattice_local[1, :] = 1
-
-    q = deque()
-    for i in range(L_local):
-        for j in range(L_local):
+    #brzegi
+    lattice[0,:] = -2
+    lattice[-1,:] = -2
+    lattice[1,:] = 1
+    
+    for i in range(L):
+        for j in range(L):
             if i == 1:
-                q.append([i, j])
-
-    return lattice_local
+                q.append([i,j])
+            else:
+                pass
+    
+    return lattice
 
 
 def check_up_down(row_index,col_index):
     if ((lattice[row_index, col_index] == -1) and (lattice[row_index, col_index] != -2)):
-        if (random_num < p):
+        if (np.random.random() < p):
             lattice[row_index, col_index] = 1
             q.append([row_index, col_index])
         else:
@@ -42,50 +39,43 @@ def check_up_down(row_index,col_index):
             
 def check_sides(row_index,col_index):
     if (lattice[row_index, col_index] == -1):
-        if (random_num < p):
+        if (np.random.random() < p):
             lattice[row_index, col_index] = 1
             q.append([row_index, col_index])
         else:
             lattice[row_index, col_index] = 0
-
-
-def leath_once(L_local, p_local):
-    global L, p, lattice, q, random_num
-
-    L = L_local
-    p = p_local
-
-    lattice = initialize_grid(L)
+    
+    
+lattice = initialize_grid(L)
 
 licznik = 0 
 while q:
     row_index, col_index = q[0]
-    random_num = np.random.random()
     
     #W dół 
-    row_index = row_index + 1
-    col_index = col_index
+    nr = row_index + 1
+    nc = col_index
         
-    check_up_down(row_index, col_index)
+    check_up_down(nr, nc)
     
         
     #Na prawo
-    row_index = row_index
-    col_index = pbc(col_index + 1)
+    nr = row_index
+    nc = pbc(col_index + 1)
         
-    check_sides(row_index, col_index)
+    check_sides(nr, nc)
 
     #Do góry 
-    row_index = row_index - 1
-    col_index = col_index
+    nr = row_index - 1
+    nc = col_index
         
-    check_up_down(row_index, col_index)
+    check_up_down(nr, nc)
     
     #Lewo
-    row_index = row_index 
-    col_index = pbc(col_index - 1)
+    nr = row_index 
+    nc = pbc(col_index - 1)
         
-    check_sides(row_index, col_index)
+    check_sides(nr, nc)
             
             
     q.popleft()
